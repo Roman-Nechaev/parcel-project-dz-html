fe2cda3f8d89e597a7f1ae89b933650108d6bcfe
Roman-Nechaev <nrchaevroman@gmail.com>
2022-11-10 11:46:36 +0200

    добавлена форма в футер

---
+++ src/js/footer-form-submit.js
@@ -0,0 +1,15 @@
+console.log('form');
+
+const footerFormRef = document.querySelector('.js-footer-form-submit');
+
+footerFormRef.addEventListener('submit', inFormSubmit);
+
+function inFormSubmit(e) {
+  e.preventDefault();
+  new FormData(e.currentTarget).forEach((value, name) => {
+    console.log(`${name}: ${value}`);
+  });
+  e.currentTarget.reset();
+}
+
+console.log('js-footer-form-submit');
