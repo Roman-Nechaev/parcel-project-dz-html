2f455bf208b665a286311c511fbe8ec71f7c71da
Roman-Nechaev <nrchaevroman@gmail.com>
2022-11-17 18:01:07 +0200

    дз-7-выполнено

--- src/js/modal.js
+++ src/js/modal.js
@@ -13,12 +13,23 @@ refs.closeModalBtn.addEventListener('click', toggleModal);
 refs.backdrop.addEventListener('click', logBacdropClick);
 
 function toggleModal() {
+  window.addEventListener('keydown', onEscKeyPress);
+
   refs.backdrop.classList.toggle('is-hidden');
 }
 
 function logBacdropClick(e) {
   if (e.currentTarget === e.target) {
     //   console.log('Клик в бекдроп');
+    toggleModal();
+  }
+}
+
+function onEscKeyPress(e) {
+  const ESC_KEY_CODE = 'Escape';
+  const isEscKey = e.code === ESC_KEY_CODE;
+
+  if (isEscKey) {
     toggleModal();
   }
 }
