54ad2c0138a5ba1c3b15bc7f62108499ad1c0ee8
Roman-Nechaev <nrchaevroman@gmail.com>
2022-11-24 18:48:06 +0200

    адаптив верстки

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 console.log('index');
+const bodyScrollLock = require('body-scroll-lock');
+const mobileMenu = document.querySelector('.js-menu-container');
+const openMenuBtn = document.querySelector('.js-open-menu');
+const closeMenuBtn = document.querySelector('.js-close-menu');
 
-// const foo = document.querySelector('#size');
+const toggleMenu = () => {
+  const isMenuOpen =
+    openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+  openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+  mobileMenu.classList.toggle('is-open');
 
-// foo.addEventListener('input', fooTest);
+  const scrollLockMethod = !isMenuOpen
+    ? 'disableBodyScroll'
+    : 'enableBodyScroll';
+  bodyScrollLock[scrollLockMethod](document.body);
+};
 
-// function fooTest(evt) {
-//   console.log(evt.target.value);
-// }
+openMenuBtn.addEventListener('click', toggleMenu);
+closeMenuBtn.addEventListener('click', toggleMenu);
+
+// Close the mobile menu on wider screens if the device orientation changes
+window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+  if (!e.matches) return;
+  mobileMenu.classList.remove('is-open');
+  openMenuBtn.setAttribute('aria-expanded', false);
+  bodyScrollLock.enableBodyScroll(document.body);
+});
