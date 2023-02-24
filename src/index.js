c4598a058a6c22d340d1e1a6ce3f7f27cf84e325
Roman-Nechaev <nrchaevroman@gmail.com>
2023-02-24 20:23:09 +0200

    fix navigation / header

--- src/index.js
+++ src/index.js
@@ -16,7 +16,6 @@ const toggleMenu = () => {
 openMenuBtn.addEventListener('click', toggleMenu);
 closeMenuBtn.addEventListener('click', toggleMenu);
 
-// Close the mobile menu on wider screens if the device orientation changes
 window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
   if (!e.matches) return;
   mobileMenu.classList.remove('is-open');
--- src/js/header-fex.js
+++ src/js/header-fex.js
@@ -1,9 +1,5 @@
-// console.log('header-fex');
-
 const { height: headerHeight } = document.querySelector('.header-fixed').getBoundingClientRect();
-// console.log(headerHeight);
 
 const mainFoo = document.querySelector('main');
-// console.log('mainFoo', mainFoo);
 
 mainFoo.style.paddingTop = `${headerHeight}px`;
